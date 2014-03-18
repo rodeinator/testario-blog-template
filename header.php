fdae78f7062b078ef91c11ce9133544a4cc0d930
Michael Rode <michael@rodein.de>
2014-03-18 14:22:28 +0100

    Removed header from menu

--- header.php
+++ header.php
@@ -22,17 +22,19 @@
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
+  <div class="container">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'testario-blog-theme' ); ?></h1>
 			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'testario-blog-theme' ); ?></a>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
+
+  </div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
