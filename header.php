82216fd2d957752c3c5452d7bd561298150269c0
Michael Rode <michael@rodein.de>
2014-03-18 14:49:19 +0100

    Use favicon.icon

--- header.php
+++ header.php
@@ -14,27 +14,27 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-	<header id="masthead" class="site-header" role="banner">
+  <header id="masthead" class="site-header" role="banner">
   <div class="container">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
+    <div class="site-branding">
+      <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+      <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+    </div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'testario-blog-theme' ); ?></a>
+    <nav id="site-navigation" class="main-navigation" role="navigation">
+      <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'testario-blog-theme' ); ?></a>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+      <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+    </nav><!-- #site-navigation -->
 
   </div>
-	</header><!-- #masthead -->
+  </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+  <div id="content" class="site-content">
